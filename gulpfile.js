92de5fe77db3ea5689771128faab578ea3b22bb6
James Buchanan <jaybitdesign@gmail.com>
2019-01-30 16:35:13 -0800

    Modified the gulpfile.js
    Compiles and minifies CSS for production/testing.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
+/* 0) cd <project dir>   */
+/* 1) npm install        */
+/* 2) npm run sass       */
+/* 3) npm run watch      */
+/* 4) Modify SCSS         */
+/* 5) Turn off watch     */
+/* ?) ????????           */
+/* 6) Profit             */
+
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const csso = require('gulp-csso')
+const flog = require('fancy-log')
 
-sass.compiler = require('node-sass')
-
-gulp.task('sass', function () {
-    return gulp.src('./sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+gulp.task('sass', function() {
+  flog('Compiling SASS w/ Gulp!')
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(csso())
+    .pipe(gulp.dest('./css'))
 })
 
-gulp.task('watch', function () {
-    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+gulp.task('watch', function() {
+  flog('Enabling SASS watch...')
+  gulp.watch('./scss/**/*.scss', gulp.series('sass'))
 })
